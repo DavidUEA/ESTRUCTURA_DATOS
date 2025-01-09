8c28d70b30fb477fa1d522106bdcbd57c19ff6b6
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2025-01-09 18:36:44 -0500

    tarea 5

--- EstructuradeDatos/Program.cs
+++ EstructuradeDatos/Program.cs
@@ -1,16 +1,21 @@
-﻿Console.WriteLine("+++++++++++++++++++++++++++++++++");
+﻿
+Console.WriteLine("+++++++++++++++++++++++++++++++++");
 Console.WriteLine("+ Universidad Estatal Amazónica +");
 Console.WriteLine("+++++++++++++++++++++++++++++++++");
-Console.WriteLine();
+Console.WriteLine();          
 Console.WriteLine("++++++ AREA DE FIGURAS ++++++++");
 Console.WriteLine();
 Console.WriteLine();
-Rectangulo rectangulo = new Rectangulo(20.5, 10.);
-Console.WriteLine("++++ Rectangulo ++++");
-Console.WriteLine("Area :" + rectangulo.Area());
-Console.WriteLine("Perimetro :");
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine("++++ Circulo++++");
-Console.WriteLine();
-Console.WriteLine();
+Rectangulo rect1 = new Rectangulo(7.5, 3.0); /// Rectangulo
+Console.WriteLine("----Rectangulo----");
+Console.WriteLine(" Area " + rect1.Area());
+Console.WriteLine(" Perimetro " + rect1.Perimetro());
+// Console.WriteLine();
+// Console.WriteLine();
+// Console.WriteLine("----Circulo----");
+// Console.WriteLine(" Area ");
+// Console.WriteLine(" Perimetro ");
+
+
+
+
