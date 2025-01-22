1c03ea44cf8d1dd0217dba6d51344dd38f9b5815
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2025-01-21 19:18:33 -0500

    Semana_6

---
+++ Semana_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EjemploLongitudLista
+{
+    public static int CalcularLongitud<T>(List<T> lista)
+    {
+        int contador = 0;
+        foreach (T elemento in lista) /// recorre cada elemento de la lista
+        {
+            contador++; /// Incrementa el contador por cada elemento
+        }
+        return contador;
+    }
+
+    public static void Main(string[] args)
+    {
+        List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 9, 11 };
+        int longitudNumeros = CalcularLongitud(numeros);
+        Console.WriteLine($"La longitud de la lista de números es: {longitudNumeros}"); // Imprime: La longitud de la lista de números es: 7
+
+        List<string> nombres = new List<string> { "Ana", "Juan", "Pedro", "David" };
+        int longitudNombres = CalcularLongitud(nombres);
+        Console.WriteLine($"La longitud de la lista de nombres es: {longitudNombres}"); // Imprime: La longitud de la lista de nombres es: 3
+
+        ///List<double> valores = new List<double>(); // Lista vacía
+        ///int longitudValores = CalcularLongitud(valores);
+       /// Console.WriteLine($"La longitud de la lista vacía es: {longitudValores}"); // Imprime: La longitud de la lista vacía es: 0///
+    }
+}
---
+++ Semana_6_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Make sure to include this for string.Join
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 9, 11 };
+        Console.WriteLine("Version original numeros:");
+        Console.WriteLine(string.Join(" ", numeros)); // string.Join(" ", numeros)) imprime todos los numeros de la lista 
+        numeros.Reverse();
+        Console.WriteLine("\nReversa de numeros:");// luego de utilizar el metodo reverse imprime los numeros al contrario 
+        Console.WriteLine(string.Join(" ", numeros)); 
+
+        List<string> nombres = new List<string> { "Ana", "Juan", "Pedro", "David" };
+        Console.WriteLine("\nVersion original nombres:");
+        Console.WriteLine(string.Join(" ", nombres)); // // string.Join(" ", nombres)) imprime todos los nombres de la lista
+        nombres.Reverse();
+        Console.WriteLine("\nReversa de nombres:");//luego de utilizar el metodo reverse imprime los numeros al contrario
+        Console.WriteLine(string.Join(" ", nombres)); 
+        Console.ReadKey();
+    }
+}
