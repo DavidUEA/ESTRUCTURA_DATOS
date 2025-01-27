9734f70d25e53c110f5c345df053343d84e4d7b2
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2025-01-26 19:56:04 -0500

    SEMANA_7

---
+++ Semana_7.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+public class TorresDeHanoi
+{
+    public static void Resolver(int n, char origen, char destino, char auxiliar)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"Mover disco 1 de {origen} a {destino}");
+            return;
+        }
+        Resolver(n - 1, origen, auxiliar, destino);
+        Console.WriteLine($"Mover disco {n} de {origen} a {destino}");
+        Resolver(n - 1, auxiliar, destino, origen);
+    }
+    public static void Main()
+    {
+        int discos = 3;
+        Resolver(discos, 'A', 'C', 'B'); // A, B, C son las torres
+    }
+}
---
+++ Semana_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+public class FormulaBalanceada
+{
+    public static bool VerificarBalance(string formula)
+    {
+        Stack<char> pila = new Stack<char>();
+        foreach (char c in formula)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                pila.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                if (pila.Count == 0) return false;
+                char apertura = pila.Pop();
+                if (!EsParejaCorrecta(apertura, c)) return false;
+            }
+        }
+        return pila.Count == 0;
+    }
+ private static bool EsParejaCorrecta(char apertura, char cierre)
+ {
+    return (apertura == '(' && cierre == ')') ||
+        (apertura == '{' && cierre == '}') ||
+        (apertura == '[' && cierre == ']');
+ }
+public static void Main()
+ {
+    string formula = "{7+(8*5)-[(9-7)+(4+1)]}";
+    Console.WriteLine("Formula balanceada: " + VerificarBalance(formula));
+ }
+}
